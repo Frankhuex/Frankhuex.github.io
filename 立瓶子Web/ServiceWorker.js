4097b54e2cb212fd9e44e047d53b0865d0a7481d
Frankhuex <HU0021AN@e.ntu.edu.sg>
2026-05-30 17:52:31 +0800

    update立瓶子

---
+++ 立瓶子Web/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-立瓶子-0.1";
+const contentToCache = [
+    "Build/立瓶子test.loader.js",
+    "Build/立瓶子test.framework.js",
+    "Build/立瓶子test.data",
+    "Build/立瓶子test.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
